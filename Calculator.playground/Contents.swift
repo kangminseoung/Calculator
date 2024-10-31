dd1501bd2aed14c6ded249b5ecb24a318ebcbc5f
kangminseoung <alstjd2356@gmail.com>
2024-10-31 21:36:49 +0900

    feat: 추상화 클래스 분리
    클래스 분리 및 각각의 클래스들을 만들고 클래스간의 관계를 고려하여 Calculator 클래스와 관계 맺기

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -5,52 +5,36 @@ class Calculator { // 클래스 이름
     
     func calculate(firstNumber: Double, secondNumber: Double, operator: Character) { // 클래스 내부 구현 func 키워드를 사용하여 파라미터 선언
         
-        func addition() -> Double { // 함수의 이름은 addition() 반환타입 Double
-            return firstNumber + secondNumber // 반환 타입 값
-        }
-
-
-        func subtraction() -> Double { // 함수의 이름은 subtraction() 반환타입 Double
-            return firstNumber - secondNumber // 반환 타입 값
-        }
-
-
-        func multiply() -> Double { // 함수의 이름은 multiply() 반환타입 Double
-            return firstNumber * secondNumber // 반환 타입 값
-        }
-
-
-
-        func divide() -> Double { // 함수의 이름은 divide() 반환타입 Double
-            return firstNumber / secondNumber // 반환 타입 값
-        }
-
-
-
-        func remainder() -> Double { // 함수의 이름은 remainder() 반환타입 Double
-            return firstNumber.truncatingRemainder(dividingBy: secondNumber) // 반환 타입 값
-        }
-
         
         // 하나만 값을 확인하기 위해 switch문을 이용해 분기
         
         switch `operator` {
         case "+":
-            print(addition())
+            var addOperator = AddOperation(firstNumber: firstNumber, secondNumber: secondNumber)
+            print(addOperator.addition())
+            
             
         case "-":
-            print(subtraction())
+            var substractOperation = Subtraction(firstNumber: firstNumber, secondNumber: secondNumber)
+            print(substractOperation.subtraction())
+            
             
         case "*":
-            print(multiply())
+            var multiplyOperation = Multiply(firstNumber: firstNumber, secondNumber: secondNumber)
+            print(multiplyOperation.multiply())
+            
             
         case "/":
-            print(divide())
+            var divideOperation = Divide(firstNumber: firstNumber, secondNumber: secondNumber)
+            print(divideOperation.divide())
+            
             
         case "%":
-            print(remainder())
+            var remainderOperation = Remainder(firstNumber: firstNumber, secondNumber: secondNumber)
+            print(remainderOperation.remainder())
             
-            default:
+            
+        default:
             print("지원을 하지 않습니다.")
             
         }
@@ -69,3 +53,81 @@ calculator.calculate(firstNumber: 7, secondNumber: 8,operator: "%")
 
 
 
+class AddOperation {
+    
+    var firstNumber: Double
+    var secondNumber: Double
+    
+    func addition() -> Double { // 함수의 이름은 addition() 반환타입 Double
+        return firstNumber + secondNumber // 반환 타입 값
+        
+        
+    }
+    init(firstNumber: Double, secondNumber: Double) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
+}
+
+class Subtraction {
+    
+    var firstNumber: Double
+    var secondNumber: Double
+    
+    func subtraction() -> Double { // 함수의 이름은 addition() 반환타입 Double
+        return firstNumber - secondNumber // 반환 타입 값
+        
+        
+    }
+    init(firstNumber: Double, secondNumber: Double) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
+}
+
+class Multiply {
+    
+    var firstNumber: Double
+    var secondNumber: Double
+    
+    func multiply() -> Double { // 함수의 이름은 addition() 반환타입 Double
+        return firstNumber * secondNumber // 반환 타입 값
+        
+        
+    }
+    init(firstNumber: Double, secondNumber: Double) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
+}
+
+class Divide {
+    
+    var firstNumber: Double
+    var secondNumber: Double
+    
+    func divide() -> Double { // 함수의 이름은 addition() 반환타입 Double
+        return firstNumber / secondNumber // 반환 타입 값
+        
+        
+    }
+    init(firstNumber: Double, secondNumber: Double) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
+}
+
+class Remainder {
+    
+    var firstNumber: Double
+    var secondNumber: Double
+    
+    func remainder() -> Double { // 함수의 이름은 addition() 반환타입 Double
+        return firstNumber.truncatingRemainder(dividingBy: secondNumber) // 반환 타입 값
+    }
+    
+    init(firstNumber: Double, secondNumber: Double) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
+}
